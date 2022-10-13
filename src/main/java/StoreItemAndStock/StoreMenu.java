7c81f18fbc9ecbca978a23eea2e858dd0f4926e7
Polobear9 <lieams1996@gmail.com>
2022-10-13 11:37:34 +0900

    Menu Set and Finish Set Menu

--- src/main/java/StoreItemAndStock/StoreMenu.java
+++ src/main/java/StoreItemAndStock/StoreMenu.java
@@ -9,27 +9,27 @@ public class StoreMenu {
     public static void main(String[] args) {
         while (true) {
             System.out.println("Enter The Menu number.");
-            System.out.println("1.");
-            System.out.println("2.");
-            System.out.println("3.");
+            System.out.println("1.Store Start");
+            System.out.println("2.Store Item Check and Stock");
+            System.out.println("3.Exit");
 
             menuId = sc.nextInt();
             switch (menuId) {
                 case 1:
                     while (true) {
-                        System.out.println("1");
+                        System.out.println("1.Store Start");
                         break;
                     }
                     break;
                 case 2:
                     while (true) {
-                        System.out.println("2");
+                        System.out.println("2.Store Item Check and Stock ");
                         break;
                     }
                     break;
                 case 3:
                     while (true) {
-                        System.out.println("3");
+                        System.out.println("3.Exit");
                         break;
                     }
                     break;

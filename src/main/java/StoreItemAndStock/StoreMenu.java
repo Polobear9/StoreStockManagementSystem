97969cdd03c20ea6c1bdb807960007666022bf54
Polobear9 <lieams1996@gmail.com>
2022-10-13 11:43:29 +0900

    sub Menu add...

--- src/main/java/StoreItemAndStock/StoreMenu.java
+++ src/main/java/StoreItemAndStock/StoreMenu.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class StoreMenu {
     static Scanner sc = new Scanner(System.in);
     static Integer menuId = 0;
+    static Integer subMenuId = 0;
 
     public static void main(String[] args) {
         while (true) {
@@ -17,13 +18,20 @@ public class StoreMenu {
             switch (menuId) {
                 case 1:
                     while (true) {
-                        System.out.println("1.Store Start");
+                        System.out.println("----Store Start----");
+                        System.out.println("Store Open");
+                        System.out.println("Store Item Stock Check");
+                        System.out.println("Store Income Check");
+                        System.out.println("Store All Sold Item Check");
+                        System.out.println("Store Close");
                         break;
                     }
                     break;
                 case 2:
                     while (true) {
-                        System.out.println("2.Store Item Check and Stock ");
+                        System.out.println("2.Store Item Check and Stock");
+                        System.out.println("Store Item Check");
+                        System.out.println("Store Item Order");
                         break;
                     }
                     break;
